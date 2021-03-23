4ed583ea24672a116ed67444d64eb73833124ffd
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-03-23 19:08:10 +0100

    Explore exceptions handling and definition

--- StatML/lib/python_crashcourse.py
+++ StatML/lib/python_crashcourse.py
@@ -511,17 +511,110 @@ print(f"d = {d}")
 a = {x for x in 'abracadabra' if x not in 'abc'}
 print(f"a = {a}")
 
-######################
-# Exceptions handling
-######################
-
-#####################################
-# Basic Operating system interfaces
-#####################################
 
 #####################################
 # Object Oriented Programming (OOP)
 #####################################
+
+
+######################
+# Exceptions handling
+######################
+
+# while True:
+#     try:
+#         x = int(input("Please enter a number: "))
+#         break
+#     except ValueError:
+#         print("Oops! That was no valid numbe. Try again...")
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+        
+try:
+    raise Exception("spam", "eggs")
+except Exception as err:
+    print(type(err))
+    print(err.args)
+    print(err)
+    
+    x, y = err.args
+    print(f"x = {x}")
+    print(f"y = {y}")
+    
+def this_fails():
+    x = 1/0
+    
+try:
+    this_fails()
+except ZeroDivisionError as err:
+    print(f"Handling run-time error: {err}")
+    
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class InputError(Error):
+    """Exception raised for errors in the input.
+    
+    Attributes:
+        expression  -- input expression in which the error occured
+        message -- explanation of the error
+    """
+    
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+        
+        
+class TransitionError(Error):
+    """Raised when an operation attempts a state transition that's not allowed.
+    
+    Attributes:
+        previous -- state at the beginning of transition
+        next -- attempted new state
+        message -- explanation of why the specific transition is not allowed.
+    """
+    
+    def __init__(self, previous, next, message):
+        self.previous = previous
+        self.next = next
+        self.message = message
+        
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("division by zero!")
+    else:
+        print("result is", result)
+    finally:
+        print("executing finally clause")
+        
+divide(2, 1)
+divide(2, 0)
+# divide("2", "1")
+    
+#####################################
+# Basic Operating system interfaces
+#####################################
+
 
 #############
 # Practicals
