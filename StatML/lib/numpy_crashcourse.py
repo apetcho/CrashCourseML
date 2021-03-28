7f2000f21aa8386f2c044fd18d49ed1fddc1f983
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-03-28 14:42:36 +0200

    Starting numpy crash course

---
+++ StatML/lib/numpy_crashcourse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+#######################
+# Numpy Crash Course
+#######################
+# 1- Create arrays
+
+# 2- Examining arrays
+
+# 3- Reshaping
+
+# 4- Stack arrays
+
+# 5- Selection
+
+# 5.1- Slicing
+
+# 5.2- Fancy indexing: Integer or boolean array indexing
+
+# 6- Vectorized operations
+
+# 7- Broadcasting
+
+# 7.1- Rules
+
+############
+# Praticals
+############
