208b24bf21b20b895ff14c69008e8b71fdcd95a6
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-03-28 15:20:40 +0200

    Specifying numpy array data types

--- StatML/lib/numpy_crashcourse.py
+++ StatML/lib/numpy_crashcourse.py
@@ -1,27 +1,54 @@
 #!/usr/bin/env python3
+import numpy as np
+
 
 #######################
 # Numpy Crash Course
 #######################
-# 1- Create arrays
+# 1- Array creation
+# 1.1 Conversion from other Python structures
 
-# 2- Examining arrays
+def create_nparray():
+    #
+    a1D = np.array([1, 2, 3, 4])
+    a2D = np.array([[1, 2], [3, 4]])
+    a3D = np.array([[[1, 2], [3, 4]],
+                    [[5, 6], [7, 8]]])
+    print(f"a1D = {a1D}\na2D = {a2D}\na3D = {a3D}")
 
-# 3- Reshaping
+create_nparray()
 
-# 4- Stack arrays
+## Specifying data type.
 
-# 5- Selection
+def nparray_dtype():
+    aInt8 = np.array([127, 128, 129], dtype=np.int8)
+    aUInt32 = np.array([2, 3, 4], dtype=np.uint32)
+    bUInt32 = np.array([5, 6, 7], dtype=np.uint32)
+    cUInt32 = aUInt32 - bUInt32
+    print(f"aInt8 = {aInt8}")
+    print(f"unsigned c: {cUInt32, cUInt32.dtype}")
+    
+nparray_dtype()
 
-# 5.1- Slicing
+# 1.2 Intrinsic NumPy array creation functions
+# 1.3 Replicating, joining, or mutating existing arrays
+# 1.4 Reading arrays from disk, either from standard or custom formats
+# 1.5 Creating arrays from raw bytes through the use of strings or buffers
+# 1.6 Use of special library functions
 
-# 5.2- Fancy indexing: Integer or boolean array indexing
+# 2- Array indexing
 
-# 6- Vectorized operations
+# 3- Numpy I/O
 
-# 7- Broadcasting
+# 4- Types
 
-# 7.1- Rules
+# 5- Broadcasting
+
+# 6- Structured Array
+
+# 7- Dispatch
+
+# 8- Subclassing
 
 ############
 # Praticals
