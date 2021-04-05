afdc9508774bb4c0e4e0ab16d49a023cfa88f092
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-04-06 01:20:59 +0200

    Play with special array creation like np.zeros, etc...

--- StatML/lib/numpy_crashcoruse.py
+++ StatML/lib/numpy_crashcoruse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
-# -- Create arrays
+## -- Create arrays
 data1 = [1, 2, 3, 4, 5]                 # list
 arr1 = np.array(data1)                  # 1d array
 data2 = [range(1, 5), range(5, 9)]      # list of lists
@@ -12,3 +12,12 @@ print(f"data2 = {data2}")
 print(f" arr2 = {arr2}")
 arr2list = arr2.tolist()                 # convert array back to list
 print(f"arr2.tolist() = {arr2list}")
+
+## -- Create special arrays
+print(f"np.zeros(10):\n{np.zeros(10)}")
+print(f"np.zeros((3, 6)):\n{np.zeros((3, 6))}")
+print(f"np.ones(10):\n{np.ones(10)}")
+# 0 to 1 (inclusive) with 5 points
+print(f"np.linspace(0, 1, 5):\n{np.linspace(0, 1, 5)}")
+# 10^0 to 10^3 (inclusive) with 4 points
+print(f"np.logspace(0, 3, 4):\n{np.logspace(0, 3, 4)}")
