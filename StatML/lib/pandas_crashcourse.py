8633233359428a799c23ace2ae4d78a771eb559d
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-04-06 12:23:20 +0200

    Play with pandas.DataFrame dataframes concatenation

--- StatML/lib/pandas_crashcourse.py
+++ StatML/lib/pandas_crashcourse.py
@@ -4,7 +4,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+section = lambda title, n: print(f"\n{'*'*n}\n{title}\n{'*'*n}\n")
+
 # Create DataFrame
+title = "Create DataFrame"
+section(title, len(title))
+
 columns = ['name', 'age', 'gender', 'job']
 user1 = pd.DataFrame([['alice', 19, 'F', 'student'],
                       ['john', 26, 'M', 'student']], columns=columns)
@@ -18,4 +23,16 @@ print(f"\nuser1:\n{user1}")
 print(f"\nuser2:\n{user2}")
 print(f"\nuser3:\n{user3}")
 
+## Combining DataFrames
+title = "Combining DataFrame"
+section(title, len(title))
 
+## -- Concatenate DataFrames
+title = "Concatenate DataFrames"
+section(title, len(title))
+
+users = user1.append(user1)
+print(f"users = user1.append(user2):\n\n{users}\n")
+
+users = pd.concat([user1, user2, user3])
+print(f"users = pd.concat([user1, user2, user3]):\n\n{users}\n")
