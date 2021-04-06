09cf2a612410e40617f515d182032279e2839b46
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-04-06 12:08:25 +0200

    Play with pandas.DataFrame creation

--- StatML/lib/pandas_crashcourse.py
+++ StatML/lib/pandas_crashcourse.py
@@ -4,3 +4,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Create DataFrame
+columns = ['name', 'age', 'gender', 'job']
+user1 = pd.DataFrame([['alice', 19, 'F', 'student'],
+                      ['john', 26, 'M', 'student']], columns=columns)
+user2 = pd.DataFrame([['eric', 22, 'M', 'student'],
+                     ['paul', 58, 'F', 'manager']], columns=columns)
+user3 = pd.DataFrame(dict(name=['peter', 'julie'],
+                          agen=[33, 44],
+                          gender=['M', 'F'],
+                          job=['engineer', 'scientist']))
+print(f"\nuser1:\n{user1}")
+print(f"\nuser2:\n{user2}")
+print(f"\nuser3:\n{user3}")
+
+
