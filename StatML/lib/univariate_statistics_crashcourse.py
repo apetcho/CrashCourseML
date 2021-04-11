8db66a9832c6c2a44687a0c9ae4b9e9c0d8a367a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-04-11 09:05:47 +0200

    Outline main points to be considered

---
+++ StatML/lib/univariate_statistics_crashcourse.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+# Estimators of the main statistical measures
+## Mean
+## Variance
+## Standard deviation
+## Covariance
+## Correlation
+## Standard Error (SE)
+
+# Main Distributions
+## Normal Distribution
+## Chi-Square distribution
+## Fisher's F-distribution
+
+# Hypothesis Testing
+## One sample t-test
+## - Model the data
+## - Fit: estimate the model parameters
+## - Test
+
+# Testing pairwise associations
+## Pearson correlation test: test association between two quantitative variables
+## Two sample (Student) t-test: compare two means
+## - Model the data
+## - Fit: estimate the model parameters
+## - T-Test
+### Equal or unequal sample sizes, unequal variances (Welch's t-test)
+## ANOVA F-test (quantitative ~ categorial (> 2 levels))
+## - Model the data
+## - Fit: estimate the model parameters
+## - F-Test
+## Chi-Square, X^2 (categorial ~ categorial)
+
+# Non-parameteric test of pairwise associations
+## Spearman rank-order correlation (quantitative ~ quantitative)
+## Wilcoxon signed-rank test (quanititative ~ categorial)
+## Mann-whitney U test (quantitative ~ categorial (2 level))
+
+# Linear model
+## Simple regression: test association between two quantitative variables
+## - Model the data
+## - Fit: estimate the model parameters
+## - F-Test
+## ** Goodness of fit
+## ** Test
+## Multiple regression
+
+# Linear model with statmodels
+## Multiple regression
+## Multiple regression with categorical independent variables or factors:
+## ... Analyis of covariance (ANCOVA)
+## - One-way AN(C)OVA
+## - Two-way AN(C)OVA
+## - Comparing two nested models
+## - Factor coding
+## - Contrasts and post-hoc tests
+
+# Multiple comparisons
+## Bonferroni correction for multiple comparisons
+## The False discovery rate (FDR) correction for multiple comparisons
+## Brain volume study
+
